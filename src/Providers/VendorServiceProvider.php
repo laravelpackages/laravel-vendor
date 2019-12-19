f43764b750d0cc465f8449fffeeccc4889c1617d
rafazingano <rafazingano@gmail.com>
2019-12-19 17:13:29 -0300

    Initial Commit

---
+++ src/Providers/VendorServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace LaravelPackages\Vendor\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class VendorServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    //protected $defer = true;
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    /*
+    public function provides()
+    {
+        return [
+            'VendorService'
+        ];
+    }
+    */
+}
